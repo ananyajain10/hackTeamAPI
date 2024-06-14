7793fcb49e2a478eae69c615341001e8682eb80f
ananyajain10 <jainananya355@gmail.com>
2024-06-14 22:17:53 +0530

    origin set

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const createTeamRoute = require('./routes/registerRoute.js');
 const cors = require('cors')
 
 const corsOptions = {
-        origin: "http://localhost:5173",
+        origin: "https://htmfront.onrender.com",
         methods: "POST",
         credentials:true,
 }
