08880a74659962c1a0a36578a4e075ddc0f02bad
ananyajain10 <jainananya355@gmail.com>
2024-06-14 15:33:39 +0530

    cors set

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const createTeamRoute = require('./routes/registerRoute.js');
+const cors = require('cors')
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+const corsOptions = {
+        origin: 'http://localhost:5173', 
+        optionsSuccessStatus: 200, 
+      }
+      
+      app.use(cors(corsOptions));
 const dotenv = require('dotenv');
 dotenv.config();
 
